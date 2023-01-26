4511947cf28f9f12682598f5e1955815f662bf7d
Michael <109736735+astapovM@users.noreply.github.com>
2023-01-26 18:24:11 +0300

    version 0.1

---
+++ main.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+
+path_for_music = '/home/infected/Рабочий стол/music from youtube'
+while True:
+    video = input('Введите ссылку на видео для конвертации в мп3 формат >>> ')
+
+    if "https://www.youtube" not in video:
+        print("Введите правильную ссылку на видео")
+    else:
+        yt = YouTube(video)
+        print(yt.title)
+        print(yt.thumbnail_url)
+        print(yt.streams.filter(only_audio=True))
+        stream = yt.streams.get_by_itag(140)
+        stream.download(output_path=path_for_music, filename=f'{yt.title}.mp3')
+
+        print('[INFO] COMPLETE!')
+
+        break
